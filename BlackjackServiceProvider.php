8b741f841cd125193f8e00aae93ec167c7c38071
Steve Harmeyer <harmeyersteve@gmail.com>
2016-10-24 15:51:31 -0700

    added routes include and commented out database migrations publishing

--- BlackjackServiceProvider.php
+++ BlackjackServiceProvider.php
@@ -9,14 +9,15 @@ class BlackjackServiceProvider extends ServiceProvider {
         $this->publishes([
             __DIR__.'/config/blackjack.php' => config_path('blackjack.php')
         ], 'config');
-
+/*
         $this->publishes([
             __DIR__.'/database/migrations/' => database_path('migrations')
         ], 'migrations');
-
+*/
         $this->publishes([
             __DIR__.'/public' => public_path('vendor/blackjack'),
         ], 'public');
+        require(__DIR__.'/routes.php');
     }
 
     public function register() {
